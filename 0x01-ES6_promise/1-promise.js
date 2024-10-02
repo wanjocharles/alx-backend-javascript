086e4234f862313fbd4e4cd59c42c4916c57cc80
root <wanjocharles98@gmail.com>
2024-10-02 08:18:09 +0000

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,12 @@
-import getFullResponseFromAPI from './1-promise';
-
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
