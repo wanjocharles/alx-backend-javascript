5276905606459058dcd1a479082be3772d33158f
root <wanjocharles98@gmail.com>
2024-10-08 06:42:08 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,13 @@
-import updateUniqueItems from "./10-update_uniq_items.js";
-import groceriesList from "./9-groceries_list.js";
+export const weakMap = new WeakMap();
 
-const map = groceriesList();
-console.log(map);
+const MAX_ENDPOINT_CALLS = 5;
 
-updateUniqueItems(map)
-console.log(map);
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
